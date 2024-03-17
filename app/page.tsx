c077f5a78bf3c24f9109d83b0966f8b0b95a769d
kota_shiratori <swan.kouta@gmail.com>
2024-03-17 22:42:31 +0900

    ♻️ 記述変更

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import BBSCardList from "./components/BBSCardList";
 import { BBSData } from "./types/types";
 
